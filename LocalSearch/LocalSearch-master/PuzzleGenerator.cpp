479993159edf64c40434d304f51e308ad6891bf6
Stella <jialuyu2024@u.northwestern.edu>
2024-05-13 14:20:15 -0500

    revise PuzzleGenerator

--- LocalSearch/LocalSearch-master/PuzzleGenerator.cpp
+++ LocalSearch/LocalSearch-master/PuzzleGenerator.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 PuzzleGenerator::PuzzleGenerator(int _nRows, int _nColumns, int _minVal, int _maxVal)
-	:nRows(_nRows), nColumns(_nColumns), minVal(_minVal), maxVal(_maxVal)
+	:nRows(_nRows), nColumns(_nColumns), minVal(_minVal), maxVal(_maxVal),
+	currBestPuzzle(_nRows, _nColumns, _minVal, _maxVal)
 {}
 
 Puzzle PuzzleGenerator::GeneratePuzzle()
@@ -11,12 +12,23 @@ Puzzle PuzzleGenerator::GeneratePuzzle()
 	maxtime = 59.9;	// To make sure we don't exceed a minute
 
 	Puzzle bestPuzzleFound = RandomWalk(4);
-
-	while (timer.GetElapsedTime() + 4 < maxtime) {
+	
+	while (timer.GetElapsedTime() + 4 < maxtime / 6) {
 		Puzzle p = RandomWalk(4);
+		p = HillClimbOnce(p);
 		if (p.GetValue() > bestPuzzleFound.GetValue()) {
 			bestPuzzleFound = p;
 		}
+	}
+
+	currBestPuzzle = bestPuzzleFound;
+
+	double elapsed = timer.GetElapsedTime();
+    double remainingTime = maxtime - elapsed;
+
+	Puzzle saPuzzle = SimulatedAnnealing(remainingTime);
+	if (saPuzzle.GetValue() > bestPuzzleFound.GetValue()) {
+		bestPuzzleFound = saPuzzle;
 	}
 
 	return bestPuzzleFound;
@@ -58,4 +70,70 @@ Puzzle PuzzleGenerator::RandomWalk(double timelimit)
 	// The following code is not executed in this function. It exists just as an example for getting all the successors of a puzzle.
 	vector<Puzzle> successors;
 	bestPuzzle.GetAllSuccessors(successors);
+}
+
+Puzzle PuzzleGenerator::SimulatedAnnealing(double timelimit) {
+	// Initialize the puzzle with the generator's configuration
+	Puzzle p = currBestPuzzle;
+	Puzzle bestPuzzle = p;
+	int bestVal = p.GetValue();
+	// Initial temperature and cooling rate
+	double T = 1000.0;
+	double coolingRate = 0.99;
+	// Define the iterations without improvement to adjust the T and cooling rate
+	int iterations = 0;
+
+	// Timer to set counterdown
+	Timer t;
+	t.StartTimer();
+	int currVal = p.GetValue();
+ 	
+	while (t.GetElapsedTime() < timelimit && T > 1) {
+		Puzzle nextPuzzle = p.GetRandomSuccessor();
+		int nextVal = nextPuzzle.GetValue();
+		// Accept the new state based on the calculated probability or if it is better
+		if (nextVal > currVal || exp((nextVal - currVal) / T) > ((double) rand() / RAND_MAX)) {
+            p = nextPuzzle;
+			currVal = nextVal;
+            if (nextVal > bestVal) {
+                bestPuzzle = nextPuzzle;
+                bestVal = nextVal;
+				iterations = 0;
+            }
+        } else {
+            iterations++;
+        }
+		//Temperature cooling to enable precise search 
+		if (iterations >= 50) {
+            T *= coolingRate;
+            iterations= 0;
+            coolingRate *= 0.99;
+        }
+		// Adjust cooling rate dynamically based on the progress
+        if (coolingRate < 0.95 && iterations < 10) {
+            coolingRate *= 1.01;
+        }
+	}
+
+	return bestPuzzle;
+}
+
+
+Puzzle PuzzleGenerator::HillClimbOnce(Puzzle startPuzzle) {
+    Puzzle currPuzzle = startPuzzle;
+    vector<Puzzle> successors;
+	// Generate and evaluate all successors of the current puzzle
+    currPuzzle.GetAllSuccessors(successors);
+    int bestVal = currPuzzle.GetValue();
+
+    for (Puzzle &succ : successors) {
+        int succValue = succ.GetValue();
+		// Move to a successor if it has a better value
+        if (succValue > bestVal) {
+            currPuzzle = succ;
+            bestVal = succValue;
+        }
+    }
+
+    return currPuzzle;
 }
--- LocalSearch/LocalSearch-master/PuzzleGenerator.h
+++ LocalSearch/LocalSearch-master/PuzzleGenerator.h
@@ -11,9 +11,12 @@ public:
 	Puzzle GeneratePuzzle();
 	Puzzle RandomWalk(double timelimit);	// Time limit given in seconds.
 	Puzzle SimulatedAnnealing(double timelimit); //added 10/20/16
+	Puzzle HillClimbOnce(Puzzle startPuzzle);
+
 	double AcceptanceProbability(int newValue, int bestValue, double T); //added 10/20/16
 private:
 	int nRows, nColumns, minVal, maxVal;
 	Timer timer;
 	double maxtime;
+	Puzzle currBestPuzzle;
 };
